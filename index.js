cd3c5e7c7d37bc33eb0c70ec46ba4a4f0f4cfa25
kws720 <kws72081@gmail.com>
2026-02-24 09:50:08 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// 변수
+let username = "김완수";
+console.log("username : " + username);
+
+username = "세하주하아빠";
+console.log("username : " + username);
+
+const arrVar = { name : "김완수", age : 40 }
+console.log("이름 : " + arrVar.name + ", 나이 : " + arrVar.age);
+arrVar.name = "세하주하아빠";
+arrVar.age = 42;
+console.log("이름 : " + arrVar.name + ", 나이 : " + arrVar.age);
